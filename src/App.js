97754be1a2209fbbd3ea21bf07257a5cb2b57efd
Ref-Bit <rsv.gho@gmail.com>
2020-04-13 17:58:46 +0300

    Ready to push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {advice: ''};
+  
+  componentDidMount(){
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios.get(`https://api.adviceslip.com/advice`)
+    .then((res) => {
+      const { advice } = res.data.slip
+      
+      this.setState({advice})
+    })
+    .catch((err) => console.log(err));
+  } 
+
+  render (){
+    const {advice} = this.state
+    return(
+      <div className="App">
+        <h1 className="title">Lit Advice</h1>
+        <div className="card">
+          <h1 className="header">{advice}</h1>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>Wanna a new advice...<span role="img" aria-label="light-bulb">💡</span></span>
+          </button>
+        </div>
+      </div>
+    )};
 }
 
 export default App;
